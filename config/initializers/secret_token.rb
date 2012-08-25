0536dd6ddc1f5c96dc9636e5d1c1a4c02b17b155
e115763 <e115763@gmail.com>
2012-08-25 11:15:33 +0900

    initial commit rails finder

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsFinder::Application.config.secret_token = 'ea167899f3c9283d8bec686be087a3e08001ada5a47c1a6783d8e0491ddeeb592cecf91fc5720e4ee62c23c10a710a959292e8c7c4f5315dd510a0ba584e438e'
