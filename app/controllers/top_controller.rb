a9202652141d258c980c4212d18f172f0ea10e78
e115763 <e115763@gmail.com>
2012-08-29 17:55:52 +0900

    show tags plain text in top/index.html.erb

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,14 @@
+# -*- coding: utf-8 -*-
+
 class TopController < ApplicationController
   def index
+    # public/resources 以下からファイル名を全取得
     @files = Dir.entries("public/resources").delete_if{|a|a.start_with?(".")}
+
+    # ファイル名からタグを取得
+    @tags = {}
+    @files.each do |file|
+      @tags[file] = TaggedFile.where({:file_name => file}).map{|a|a.tag}
+    end
   end
 end
