bd18db9f7b0c86645464b21df793ef3ef66c54b9
e115763 <e115763@gmail.com>
2012-08-30 21:09:32 +0900

    split tag search function in 'tag_search' method

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -15,29 +15,35 @@ class TopController < ApplicationController
     # 検索(@filesを絞り込む)
     case params[:radio_param][:search_type]
     when "file"
-      # ファイル名指定
+      # ファイル名指定検索
       @files.select!{|file|file.include?(params[:keyword])}
     when "tag"
-      # タグ指定
-      selected_tag = TaggedFile.where("tag like '%#{params[:keyword]}%'")
-
-      # タグが見つからない場合
-      if selected_tag.empty?
-        @files = []
-        return
-      end
-
-      # 検索対象のタグを持ってるファイル名一覧を作る
-      selected_tag.map!{|a|a.file_name}
-
-      @files.select! do |file|
-        return false if selected_tag.empty?
-        selected_tag.include?(file)
-      end
-
+      # タグ指定検索
+      tag_search params[:keyword]
     else
       # 何も無し
     end
 
+  end
+
+  private
+
+  def tag_search pattern
+    # patternを含むタグを持っているファイル名の配列を@filesに入れる
+    selected_tag = TaggedFile.where("tag like '%#{pattern}%'")
+
+    # タグが見つからない場合
+    if selected_tag.empty?
+      @files = []
+      return
+    end
+
+    # 検索対象のタグを持ってるファイル名一覧を作る
+    selected_tag.map!{|a|a.file_name}
+
+    @files.select! do |file|
+      return false if selected_tag.empty?
+      selected_tag.include?(file)
+    end
   end
 end
