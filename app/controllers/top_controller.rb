5f14971811368bc2c234b630f21f1a477704a377
e115763 <e115763@gmail.com>
2012-08-30 21:47:03 +0900

    split function get files array in public/resources into 'resource_list' method

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,26 +2,24 @@
 
 class TopController < ApplicationController
   def index
-    # public/resources 以下からファイル名を全取得
-    @files = Dir.entries("public/resources").delete_if{|a|a.start_with?(".")}
 
-    # ファイル名からタグを取得
-    @tags = {}
-    @files.each do |file|
-      @tags[file] = TaggedFile.where({:file_name => file}).map{|a|a.tag}
+    # params[:radio_param]が無いなら、全てのデータを表示
+    if params[:radio_param].nil?
+      @files = resources_list
+      @tags = collect_tags @files
+      return
     end
-
-    # params が無いなら、全てのデータを表示したいのででここで終わる
-    return if params[:radio_param].nil?
 
     # 検索(@filesを絞り込む)
     case params[:radio_param][:search_type]
     when "file"
       # ファイル名指定検索
       @files.select!{|file|file.include?(params[:keyword])}
+      @tags = collect_tags @files
     when "tag"
       # タグ指定検索
-      tag_search params[:keyword]
+      @files = tag_search params[:keyword]
+      @tags = collect_tags @files
     else
       # 何も無し
     end
@@ -29,22 +27,41 @@ class TopController < ApplicationController
 
   private
 
+  def resources_list
+    # public/resources 以下のファイル名をすべて返す
+    Dir.entries("public/resources").delete_if{|a|a.start_with?(".")}
+  end
+
+  def collect_tags files
+    # files 全てから(file_name=>tagの配列)のハッシュを返す
+
+    return {} if files.nil?
+
+    tags = {}
+    files.each do |file|
+      tags[file] = TaggedFile.where({:file_name => file}).map{|a|a.tag}
+    end
+    tags
+  end
+
   def tag_search pattern
-    # patternを含むタグを持っているファイル名の配列を@filesに入れる
+    # patternを含むタグを持っているファイル名の配列を返す
     selected_tag = TaggedFile.where("tag like '%#{pattern}%'")
 
     # タグが見つからない場合
-    if selected_tag.empty?
-      @files = []
-      return
-    end
+    return [] if selected_tag.empty?
 
-    # 検索対象のタグを持ってるファイル名一覧を作る
+    # 検索対象のタグを持ってるファイル名配列を作る
     selected_tag.map!{|a|a.file_name}
 
-    @files.select! do |file|
-      return false if selected_tag.empty?
+    # タグを持ってるファイル名配列に存在ものだけ残す
+    files = resources_list
+    return [] if files.empty?
+
+    files.select! do |file|
       selected_tag.include?(file)
     end
+    files
   end
+
 end
