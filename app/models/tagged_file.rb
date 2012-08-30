248285a2efe4d82be9442d6f24f77ffed57843ea
e115763 <e115763@gmail.com>
2012-08-30 10:27:37 +0900

    add validation in model/tagged_file.rb

--- app/models/tagged_file.rb
+++ app/models/tagged_file.rb
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+
 class TaggedFile < ActiveRecord::Base
   attr_accessible :file_name, :tag
+
+  # ファイル名は必ず入力
+  validates :file_name, :presence => true
+
+  # タグも必ず入力
+  # 加えてファイル名+タグの組み合わせは重複を許さない
+  validates :tag, :presence => true, :uniqueness => {:scope => [:file_name,:tag]}
 end
