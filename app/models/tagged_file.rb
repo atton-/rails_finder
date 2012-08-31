4cf8ab56281afa4eb59e08f8472ee62b6d255d2b
e115763 <e115763@gmail.com>
2012-08-31 22:54:42 +0900

    mv TaggedFile class method define

--- app/models/tagged_file.rb
+++ app/models/tagged_file.rb
@@ -10,61 +10,61 @@ class TaggedFile < ActiveRecord::Base
   # 加えてファイル名+タグの組み合わせは重複を許さない
   validates :tag, :presence => true, :uniqueness => {:scope => [:file_name,:tag]}
 
-end
 
-# TaggedFileのクラスメソッド
-def TaggedFile.resources_list
-  # public/resources 以下のファイル名をすべて返す
-  Dir.entries("public/resources").delete_if{|a|a.start_with?(".")}
-end
-
-def TaggedFile.collect_tags files
-  # files 全てに対応した(file_name=>tagの配列)のハッシュを返す
-
-  return {} if files.nil?
-
-  all_tag = TaggedFile.all
-
-  tags = {}
-  files.each do |file|
-    tags[file] = all_tag.select{|a|a.file_name == file}.map{|a|a.tag}
+  # TaggedFileのクラスメソッド
+  def self.resources_list
+    # public/resources 以下のファイル名をすべて返す
+    Dir.entries("public/resources").delete_if{|a|a.start_with?(".")}
   end
-  tags
-end
 
-def TaggedFile.file_name_search patterns
-  # patterns を全て含むファイル名の配列を返す
-  # patterns はスペースで区切られた検索patternの文字列を想定
-  resources_list.select do |file|
-    patterns.split(" ").all? do |pattern|
-      file.include? pattern
+  def self.collect_tags files
+    # files 全てに対応した(file_name=>tagの配列)のハッシュを返す
+
+    return {} if files.nil?
+
+    all_tag = TaggedFile.all
+
+    tags = {}
+    files.each do |file|
+      tags[file] = all_tag.select{|a|a.file_name == file}.map{|a|a.tag}
+    end
+    tags
+  end
+
+  def self.file_name_search patterns
+    # patterns を全て含むファイル名の配列を返す
+    # patterns はスペースで区切られた検索patternの文字列を想定
+    resources_list.select do |file|
+      patterns.split(" ").all? do |pattern|
+        file.include? pattern
+      end
     end
   end
-end
 
-def TaggedFile.tag_search patterns
-  # patterns を全て含むタグを持っているファイル名の配列を返す
-  # patterns はスペースで区切られた検索patternの文字列を想定
+  def self.tag_search patterns
+    # patterns を全て含むタグを持っているファイル名の配列を返す
+    # patterns はスペースで区切られた検索patternの文字列を想定
 
-  # pattern にマッチするリストを全部取得
-  conditions = patterns.split(" ").map{|pattern|"tag like '%#{pattern}%'"}
-  pattern_matched_files_list = conditions.map do |cond|
-    TaggedFile.select("distinct file_name").where(cond)
+    # pattern にマッチするリストを全部取得
+    conditions = patterns.split(" ").map{|pattern|"tag like '%#{pattern}%'"}
+    pattern_matched_files_list = conditions.map do |cond|
+      self.select("distinct file_name").where(cond)
+    end
+    pattern_matched_files_list.map!{|list|list.map!{|file|file.file_name}}
+
+    # 全てのpatternsをtagsに持つファイルのみ残す。(配列の積を使ってる)
+    all_files = TaggedFile.select("distinct file_name").map{|a|a.file_name}
+    patterns_matched_files = pattern_matched_files_list.inject(all_files) {|all,n|all & n}
+
+    # patternを含むタグが見つからない場合は空の配列を返す
+    return [] if patterns_matched_files.empty?
+
+    # タグを持っているファイル名のみ配列に残す
+    files = resources_list
+    return [] if files.empty?
+    files.select! do |file|
+      patterns_matched_files.any?{|tag| tag.include?(file)}
+    end
+    files
   end
-  pattern_matched_files_list.map!{|list|list.map!{|file|file.file_name}}
-
-  # 全てのpatternsをtagsに持つファイルのみ残す。(配列の積を使ってる)
-  all_files = TaggedFile.select("distinct file_name").map{|a|a.file_name}
-  patterns_matched_files = pattern_matched_files_list.inject(all_files) {|all,n|all & n}
-
-  # patternを含むタグが見つからない場合は空の配列を返す
-  return [] if patterns_matched_files.empty?
-
-  # タグを持っているファイル名のみ配列に残す
-  files = resources_list
-  return [] if files.empty?
-  files.select! do |file|
-    patterns_matched_files.any?{|tag| tag.include?(file)}
-  end
-  files
 end
