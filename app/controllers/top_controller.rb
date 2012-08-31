d291c35223fa4baa3fb64eb6bea423beb0169e45
e115763 <e115763@gmail.com>
2012-08-31 22:40:20 +0900

    support invalid post params in top controller

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -16,13 +16,18 @@ class TopController < ApplicationController
       # ファイル名指定検索
       @files = TaggedFile.file_name_search params[:keywords]
       @tags = TaggedFile.collect_tags @files
+      return
     when "tag"
       # タグ指定検索
       @files = TaggedFile.tag_search params[:keywords]
       @tags = TaggedFile.collect_tags @files
+      return
     else
-      # 何も無し
+      # 何も無しの場合
     end
-  end
 
+    # どの条件にも値しないparamsを渡された時の対策用に空のデータ
+    @files = []
+    @tags = {}
+  end
 end
